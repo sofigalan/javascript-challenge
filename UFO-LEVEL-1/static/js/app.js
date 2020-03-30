f8f07f67d0a49f2d82a652e771eb4ea14efa4a3b
unknown <sofigalan@gmail.com>
2020-03-29 19:41:07 -0600

    Changes

--- UFO-LEVEL-1/static/js/app.js
+++ UFO-LEVEL-1/static/js/app.js
@@ -2,29 +2,25 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-
-// Use D3 to select the table body
+// Select tbody
 var tbody = d3.select("tbody");
-// Use D3 to select the table
+// Selec table
 var table = d3.select("table");
-// YOUR CODE HERE!
+
 data.forEach((UFO_Report) => {
   var row = tbody.append("tr");
   Object.entries(UFO_Report).forEach(([key, value]) => {
     var cell = row.append("td");
     cell.text(value);
   });
-//console.log(UFO_Report)
 });
 
-// Select the button
+// Select button
 var button = d3.select("#filter-btn");
-//var tbody=d3.select("tbody");
+
 button.on("click", function() {
   tbody.selectAll("tr").remove();
-  // Select the input element and get the raw HTML node
   var inputElement = d3.select("#datetime");
-  // Get the value property of the input element
   var inputValue = inputElement.property("value");
   console.log(inputValue);
 
